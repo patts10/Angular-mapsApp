b4f512714c8542e07c60480e6b362a0909e8f28b
Christian Mendoza <patts2810@gmail.com>
2021-10-24 18:42:32 -0500

    feat: make routes system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { MapasModule } from './mapas/mapas.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MapasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
