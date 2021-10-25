725ae6aafa84ba72b7f7636a98ff70fac0037c18
Christian Mendoza <patts2810@gmail.com>
2021-10-24 19:15:15 -0500

    wip: create aplication menu

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  ruta: string;
+  nombre: string; 
+}
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+    `
+    li {
+      cursor: pointer;
+    }
+    `
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  menuItems: MenuItem[] = [
+    {
+      ruta: 'mapas/full-screen',
+      nombre: 'Fullscreen'
+    },
+    {
+      ruta: 'mapas/zoom-range',
+      nombre: 'Zoomrange'
+    },
+    {
+      ruta: 'mapas/propiedades',
+      nombre: 'Propiedades'
+    },
+    {
+      ruta: 'mapas/marcadores',
+      nombre: 'Marcadores'
+    }
+  ]
 }
